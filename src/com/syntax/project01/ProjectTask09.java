9f691e3a96220c9bdeeb0a5430e141c6d9199946
guccinel <106288285+guccinel@users.noreply.github.com>
2022-06-01 22:05:54 -0400

    Update ProjectTask09.java

--- src/com/syntax/project01/ProjectTask09.java
+++ src/com/syntax/project01/ProjectTask09.java
@@ -4,13 +4,17 @@ public class ProjectTask09 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] num = { 2, 15, 3500, 1, 80, 20 };
-		int max = 0;
-		for (int i = 0; i < num.length; i++) {
-			if (max < num[i]) {
-				max = num[i];
-			}
-		} 
-   System.out.println(max);
-}
-}
+		int[] array= {21,52,13,58,49,165,147};
+		
+		int max=array[0];
+		int min=array[0];
+		
+		for(int i=0;i<array.length;i++) {
+			if(array[i]<min)min=array[i];
+			if(array[i]>max)max=array[i];
+		}
+		System.out.println("The smallest value in the array is "+min);
+		System.out.println("The largest value in the array is "+max);
+		
+		
+	}
